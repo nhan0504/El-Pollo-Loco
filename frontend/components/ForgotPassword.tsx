fe777aab324f06b7b87bc1b17c703d79f2da284f
sean <sdondley81@gmail.com>
2024-04-04 17:24:23 -0400

    change signin form to password reset form

--- frontend/components/ForgotPassword.tsx
+++ frontend/components/ForgotPassword.tsx
@@ -34,13 +34,11 @@ export default function ForgotPassword() {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get('email'), 
     });
   };
 
-  // TODO customize signin form to have forgotpassword fields
-  return ( //TODO THEME
+  return (
     //<ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
@@ -56,7 +54,7 @@ export default function ForgotPassword() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Reset Password
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
@@ -69,37 +67,18 @@ export default function ForgotPassword() {
               autoComplete="email"
               autoFocus
             />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Send Reset Link
             </Button>
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                  Remember your password? Sign In
                 </Link>
               </Grid>
             </Grid>
