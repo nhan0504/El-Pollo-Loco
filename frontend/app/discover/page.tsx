5083fdaccc64207fe36e9d27d5e16d427ae5db81
lila-masand <80842035+lila-masand@users.noreply.github.com>
2024-04-09 12:27:03 -0400

    Separated navigation bar and feed buttons into separate .tsx files

--- frontend/app/discover/page.tsx
+++ frontend/app/discover/page.tsx
@@ -1,10 +1,12 @@
 import PrimarySearchAppBar from "@/components/navigation";
 import OutlinedCard from "@/components/pollCard";   
+import FeedButtons from "@/components/feedButtons";
 
 export default function Navigation() {
     return (
         <div>
         <PrimarySearchAppBar />
+		<FeedButtons />
         <OutlinedCard />
         </div>
         
