c741a5e206fec0302e29557fc1add04e732d1e9e
ammuiyer <ammu.iyer5@gmail.com>
2024-04-12 14:09:42 -0400

    made pollcards more variable-y

--- frontend/components/pollCard.tsx
+++ frontend/components/pollCard.tsx
@@ -6,80 +6,39 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import LinearProgress from '@mui/material/LinearProgress';
+import { ButtonGroup } from '@mui/material';
 
-
-const card = (
+const makeCard = (tags:Array<string>, question: string, opts: Array<string>, optValues: Array<number>) => (
   <React.Fragment>
         <Card style={{display: 'flex', justifyContent: 'space-evenly', flexDirection: 'column'}}>
         <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Would you rather?
-          </Typography>
+          
+              <ButtonGroup variant="contained" aria-label="Basic button group">
+              {tags.map(tag => <Button key={tag}>{tag}</Button>)}
+              </ButtonGroup>
+          
           <br />
-          <Typography variant="h5" component="div">
-            Would you rather have super strength or be invisible?
+          <Typography variant="h5" component="div" >
+            {question}
           </Typography>
             <br />
-            </CardContent>
-            <CardActions>
-              <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>Super Strength!</Button>
-              <Box sx={{ width: 3/4, boxShadow: 1}}>
-                <LinearProgress variant="determinate" value={55} />
-              </Box>
-            </CardActions>
-            <CardActions>
-              <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>Invisibility!</Button>
-              <Box sx={{ width: 3/4, boxShadow: 1}}>
-                <LinearProgress variant="determinate" value={45} />
-              </Box>
-            </CardActions>
+        </CardContent>
+            {opts.map((option, index) => 
+                <CardActions key={option}>
+                    <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>{option}</Button>
+                    <Box sx={{ width: 3/4, boxShadow: 1}}>
+                        <LinearProgress variant="determinate" value={optValues[index]} />
+                    </Box>
+                </CardActions>
+            )}
             </Card>
   </React.Fragment>
 );
 
-const card2 = (
-  <React.Fragment>
-        <Card style={{display: 'flex', justifyContent: 'space-evenly', flexDirection: 'column'}}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Movies!
-          </Typography>
-          <br />
-          <Typography variant="h5" component="div">
-            Which Lord of the Rings movie is the best?
-          </Typography>
-            <br />
-            </CardContent>
-            <CardActions>
-              <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>Fellowship of the Ring</Button>
-              <Box sx={{ width: 3/4, boxShadow: 1}}>
-                <LinearProgress variant="determinate" value={40} />
-              </Box>
-            </CardActions>
-            <CardActions>
-              <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>The Two Towers</Button>
-              <Box sx={{ width: 3/4, boxShadow: 1}}>
-                <LinearProgress variant="determinate" value={30} />
-              </Box>
-            </CardActions>
-            <CardActions>
-              <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>Return of the King</Button>
-              <Box sx={{ width: 3/4, boxShadow: 1}}>
-                <LinearProgress variant="determinate" value={30} />
-              </Box>
-            </CardActions>
-            </Card>
-  </React.Fragment>
-);
-
-
-
-
-
-export default function OutlinedCard() {
+export default function PollCard(tags:Array<string>, question: string, opts: Array<string>, optValues: Array<number>) {
   return (
     <Box sx={{ minWidth: 275 }}>
-      <Card variant="outlined">{card}</Card>
+      <Card variant="outlined">{makeCard(tags, question, opts, optValues)}</Card>
     </Box>
   );
 }
--- frontend/components/pollCards.tsx
+++ frontend/components/pollCards.tsx
@@ -5,58 +5,20 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { ButtonGroup, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import LinearProgress from '@mui/material/LinearProgress';
 import PollCard from './pollCard';
 
 
 
-const card2 = (tags:Array<string>, question: string, opts: Array<string>, optValues: Array<number>) => (
-  <React.Fragment>
-        <Card style={{display: 'flex', justifyContent: 'space-evenly', flexDirection: 'column'}}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Tags: 
-            <ButtonGroup variant="contained" aria-label="Basic button group">
-            {tags.map(tag => <Button key={tag}>{tag}</Button>)}
-            </ButtonGroup>
-          </Typography>
-          <br />
-          <Typography variant="h5" component="div">
-            {question}
-          </Typography>
-            <br />
-            </CardContent>
-            <CardActions>
-              <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>Fellowship of the Ring</Button>
-              <Box sx={{ width: 3/4, boxShadow: 1}}>
-                <LinearProgress variant="determinate" value={40} />
-              </Box>
-            </CardActions>
-            <CardActions>
-              <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>The Two Towers</Button>
-              <Box sx={{ width: 3/4, boxShadow: 1}}>
-                <LinearProgress variant="determinate" value={30} />
-              </Box>
-            </CardActions>
-            <CardActions>
-              <Button variant="contained" style={{maxWidth: '30%', maxHeight: '30%', minWidth: '30%', minHeight: '30%'}}>Return of the King</Button>
-              <Box sx={{ width: 3/4, boxShadow: 1}}>
-                <LinearProgress variant="determinate" value={30} />
-              </Box>
-            </CardActions>
-            </Card>
-  </React.Fragment>
-);
-
 function FormRow() {
   return (
     <React.Fragment>
       <Grid item xs={4} style={{padding: 50}}>
-        {card2(["Movies", "Lord of the rings"], "Which Lord of The rings?", ["The first", "The second", "The third"], [30, 20, 50])}
+        {PollCard(["Movies", "Lord of the rings"], "Which Lord of The rings?", ["The first", "The second", "The third"], [30, 20, 50])}
       </Grid>
       <Grid item xs={4} style={{padding: 50}}>
-        {}
+        {PollCard(["Movies", "Lord of the rings"], "Which Lord of The rings?", ["The first", "The second", "The third"], [30, 20, 50])}
       </Grid>
     </React.Fragment>
   );
