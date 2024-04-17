5e523002ad7089375596dcb3446215c200798f78
sean <sdondley81@gmail.com>
2024-04-17 15:26:09 -0400

    add fixed discover page, loads 6 polls

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pollsRouter = require('./routes/polls');
+var feedRouter = require('./routes/feed');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/polls', pollsRouter);
+app.use('/feed', feedRouter);
 
 
 // catch 404 and forward to error handler
