6db16fe7314c7e46a6bf56fe9ffe1526a397a144
nhan0504 <76593076+nhan0504@users.noreply.github.com>
2024-04-28 11:04:58 -0700

    Modify posting poll endpoint to add tags (#11)

--- backend/routes/polls/index.js
+++ backend/routes/polls/index.js
@@ -7,6 +7,7 @@ const checkAuthenticated = require('../../middleware.js');
 const voteRouter = require('./vote');
 const commentRouter = require('./comment');
 
+// GET
 router.get('/:pollId', function (req, res) {
   const pollId = req.params.pollId;
   pool.query('SELECT * FROM Polls WHERE poll_id = ?', [pollId], (error, pollResults) => {
@@ -36,9 +37,11 @@ router.get('/:pollId', function (req, res) {
   });
 });
 
-router.post('/', checkAuthenticated, function (req, res) {
-  const { title, options } = req.body;
+// POST
+router.post('/', checkAuthenticated, async (req, res) => {
+  const { title, options, tags } = req.body;
   const userId = req.user.user_id;
+
   pool.query(
     'INSERT INTO Polls(user_id, title, created_at) VALUES (?,?,NOW())',
     [userId, title],
@@ -48,25 +51,78 @@ router.post('/', checkAuthenticated, function (req, res) {
         res.status(500).send('Error creating poll');
         return;
       }
-      const pollId = results.insertId;
-      const optionsData = options.map((option) => [pollId, option]);
 
-      pool.query(
-        'INSERT INTO Options(poll_id, option_text) VALUES ?',
-        [optionsData],
-        (error, results) => {
+      const pollId = results.insertId;
+
+      if (insertOption(pollId, options) != 0 || insertTag(pollId, tags) != 0) {
+        res.status(500).send('Error creating poll');
+        pool.query('DELETE FROM Polls WHERE poll_id = ?', [pollId], (error, results) => {
           if (error) {
-            console.error(`Error adding options to poll ${pollId}`, error);
-            res.status(500).send('Error adding options to the poll');
-            return;
+            console.error(`Error deleting poll ${pollId}`, error);
+            res.status(500).send('Error creating poll');
           }
-          res.status(201).send(`Poll created successfully with ID ${pollId}`);
-        },
-      );
-    },
+        });
+        return; 
+      } 
+
+      res.status(201).send(`Poll created successfully with ID ${pollId}`);
+            
+    }
   );
 });
 
+function insertOption(pollId, options) {
+  const optionsData = options.map((option) => [pollId, option]);
+  pool.query(
+    'INSERT INTO Options(poll_id, option_text) VALUES (?)',
+    [optionsData],
+    (error, result) => {
+      if(error) {
+        console.log(`Error adding option`, error);
+        return 1;
+      }
+    }
+  );
+  return 0;
+}
+
+function insertTag(pollId, tags) {
+  for (const tagName of tags) {
+    let tagId;
+
+    pool.query('SELECT tag_id FROM Tags WHERE tag_name = ?', [tagName], (error, result) => { 
+      if (error) {
+        console.log('Error getting existing tag', error);
+        return 1;
+      }
+      if (result.length != 0) {
+        tagId = result[0].tag_id;
+      } else {
+        pool.query('INSERT INTO Tags(tag_name) VALUES (?)', [tagName], (error, result) => {
+          if (error) {
+            console.log('Error inserting tag', error);
+            return 1;
+          }
+          tagId = result.insertId;
+        });  
+      }
+
+      pool.query(
+        'INSERT INTO PollsTags(tag_id, poll_id) VALUES (?, ?)',
+        [tagId, pollId],
+        (error, results) => {
+          if (error) {
+            console.log(error);
+            return 1;
+          }     
+        }
+      );
+    })
+  }
+  return 0;
+}
+
+// DELETE
 router.delete('/:pollId', checkAuthenticated, function (req, res) {
   const pollId = req.params.pollId;
   pool.query('DELETE FROM Polls WHERE poll_id = ?', [pollId], (error, results) => {
