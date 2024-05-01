bcee5a8c1e085241ff215d087add3c3f6769a5b9
ammuiyer <ammu.iyer5@gmail.com>
2024-05-01 15:47:45 -0400

    Merge branch 'main' of https://github.com/sowrathi-somasundaram/El-Pollo-Loco into comments

--- frontend/components/discover/comments.tsx
+++ frontend/components/discover/comments.tsx
@@ -35,8 +35,7 @@ export default function CommentBox(tags: string[], question: string, opts: { opt
     <React.Fragment>
       <Button variant="outlined" color="neutral" onClick={()=>{
         setOpen(true)
-        comments = CommentsWPoll(tags, question, opts, username, pollId)
-        alert(comments);
+        comments = parent(tags, question, opts, username, pollId)
       }}>
         <Stack>
           <CommentIcon/>
@@ -74,7 +73,7 @@ export default function CommentBox(tags: string[], question: string, opts: { opt
             Comments
           </Typography>
           <Typography id="modal-desc" textColor="text.tertiary"> 
-            {CommentsWPoll(tags, question, opts, username, pollId)}
+            {parent(tags, question, opts, username, pollId)}
           </Typography>
         </Sheet>
         </ModalDialog>
@@ -83,47 +82,128 @@ export default function CommentBox(tags: string[], question: string, opts: { opt
   );
 }
 
-function getComments(pollId: number) {
+function parent(tags: string[], question: string, opts: { optionText: string; votes: number; option_id: number; }[], username: string, pollId: number){
+  let [cmts, setCmts] = React.useState([])
+  function getComments(pollId: number) {
   
-   
-
-  // fetch(`${process.env.BACKEND_URL}/polls/comment/44`, {
-  //   method: 'GET',
-  //   credentials: 'include',
-  //   headers: { 'Content-Type': 'application/json' }
-  // })
-  //   .then((response) => {
-  //     if (!response.ok) {
-  //       return response.text().then((text) => {
-  //         throw new Error(text);
-  //       });
-  //     } else {
-  //       return response.text();
-  //     }
-  //   })
-  //   .catch((error) => alert(error.message));
-
-  return [{}, {}, {}];
+     
   
-}
+    fetch(`${process.env.BACKEND_URL}/polls/comment/96`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.text().then((text) => {
+            throw new Error(text);
+          });
+        } else {
+          response.json().then((re)=>{
+            // alert(re)
+            setCmts(re)
+            return re
+          });
+        }
+      })
+      .catch((error) => alert(error.message));
+      
+      
+  }
+    
   
-
-
-function Comment(data: any) {
-  return (
-    <Paper style={{ padding: "20px 10px"}} elevation={3}>
-    <Grid container  spacing={2}>
-      <Grid item justifyContent='center'>
-        <Avatar alt="Remy Sharp" />
+  
+  function Comment(data: any) {
+    return (
+      <Paper style={{ padding: "20px 10px"}} elevation={3}>
+      <Grid container  spacing={2}>
+        <Grid item justifyContent='center'>
+          <Avatar alt="Remy Sharp" />
+        </Grid>
+        <Grid item xs>
+          <h4 style={{ margin: 2, textAlign: 'left' }}>sillyUser</h4> 
+          <p style={{ textAlign: 'left' }}>{data.comment} </p>
+        </Grid>
       </Grid>
-      <Grid item xs>
-        <h4 style={{ margin: 2, textAlign: 'left' }}>sillyUser</h4> 
-        <p style={{ textAlign: 'left' }}>"contraversial take" </p>
+      </Paper>
+    );
+  }
+
+  function Comments(pollId: number, cmts: any) {
+    //wrapped up in the same paper means they r replies to each other, seperate papers r seperate comments
+    let listOfComments: any = []
+    console.log("in the comments function")
+    if( cmts.length>0){
+      listOfComments.push(<React.Fragment>
+        {Comment(cmts[0])}
+      </React.Fragment>);
+    }
+    for (let i = 1; i < cmts.length; i++) {
+      listOfComments.push(<React.Fragment>
+        <Divider variant="fullWidth" style={{ margin: '5px 0' }}/>
+        {Comment(cmts[i])}
+      </React.Fragment>)
+    }
+    return (
+      <Paper style={{ maxHeight: 400,
+      minWidth: 'min-content', overflow: 'auto' }}>
+        <List>
+          {listOfComments}
+        </List>
+      </Paper>
+    );
+  }
+  
+  function AddComment(cmts: {}[]){
+    let [currComment, setCurrComment] = React.useState("")
+    return (
+      <React.Fragment>
+        <Paper style={{ padding: "20px 10px"}} elevation={3}>
+      <Grid container  spacing={2}>
+        <Grid item justifyContent='center'>
+          <Avatar alt="Remy Sharp" />
+        </Grid>
+        <Grid item xs>
+          <h4 style={{ margin: 2, textAlign: 'left' }}>sillyUser</h4> 
+          <TextField id="filled-basic" label="Write a comment..." variant="filled" fullWidth onKeyDown={(ev) => {
+      
+      if (ev.key === 'Enter') {
+        ev.preventDefault();
+        //make new Comment
+        //should add to the databse too
+        cmts.push(Comment(currComment))
+  
+      }
+    }}  onChange={(ev)=>{setCurrComment(ev.target.value)}}/>
+        </Grid>
       </Grid>
-    </Grid>
-    </Paper>
+      </Paper>
+      </React.Fragment>
   );
+  }
+  
+  function CommentsWPoll(tags: string[], question: string, opts: { optionText: string; votes: number; option_id: number; }[], username: string, pollId: number){
+    let { innerWidth: width, innerHeight: height } = window;
+    width = (width)*0.5
+    getComments(pollId);
+    //load until comments is not empty
+    if (cmts.length==0){
+      
+    }
+    return (
+      <Paper style={{ minHeight: 'fit-content',
+        minWidth: width, overflow: 'auto' }}>
+            {PollCard(tags, question, opts, username)}
+            {Comments(pollId, cmts)}
+            {/* {AddComment(cmts)} */}
+            
+        </Paper>
+    );
+  }
+
+  return CommentsWPoll(tags, question, opts, username, pollId);
 }
+
 function NoComments(){
   return (
     <Paper style={{ padding: "40px 20px" }}>
@@ -140,69 +220,5 @@ function NoComments(){
   );
 }
 
-function Comments(pollId: number, cmts: any) {
-  //wrapped up in the same paper means they r replies to each other, seperate papers r seperate comments
-  let listOfComments: any = []
-  if( cmts.length>0){
-    listOfComments.push(<React.Fragment>
-      {Comment(cmts[0])}
-    </React.Fragment>);
-  }
-  for (let i = 1; i < cmts.length; i++) {
-    listOfComments.push(<React.Fragment>
-      <Divider variant="fullWidth" style={{ margin: '5px 0' }}/>
-      {Comment(cmts[i])}
-    </React.Fragment>)
-  }
-  return (
-    <Paper style={{ maxHeight: 400,
-    minWidth: 'min-content', overflow: 'auto' }}>
-      <List>
-        {listOfComments}
-      </List>
-    </Paper>
-  );
-}
 
-function AddComment(cmts: {}[]){
-  let [currComment, setCurrComment] = React.useState("")
-  return (
-    <React.Fragment>
-      <Paper style={{ padding: "20px 10px"}} elevation={3}>
-    <Grid container  spacing={2}>
-      <Grid item justifyContent='center'>
-        <Avatar alt="Remy Sharp" />
-      </Grid>
-      <Grid item xs>
-        <h4 style={{ margin: 2, textAlign: 'left' }}>sillyUser</h4> 
-        <TextField id="filled-basic" label="Write a comment..." variant="filled" fullWidth onKeyDown={(ev) => {
-    
-    if (ev.key === 'Enter') {
-      ev.preventDefault();
-      //make new Comment
-      //should add to the databse too
-      cmts.push(Comment(currComment))
 
-    }
-  }}  onChange={(ev)=>{setCurrComment(ev.target.value)}}/>
-      </Grid>
-    </Grid>
-    </Paper>
-    </React.Fragment>
-);
-}
-
-function CommentsWPoll(tags: string[], question: string, opts: { optionText: string; votes: number; option_id: number; }[], username: string, pollId: number){
-  let { innerWidth: width, innerHeight: height } = window;
-  width = (width)*0.5
-  let cmts = getComments(pollId);
-  return (
-    <Paper style={{ minHeight: 'fit-content',
-      minWidth: width, overflow: 'auto' }}>
-          {PollCard(tags, question, opts, username)}
-          {Comments(pollId, cmts)}
-          {AddComment(cmts)}
-          
-      </Paper>
-  );
-}
