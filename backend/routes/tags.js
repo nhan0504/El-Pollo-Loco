9065fe605d86aa4677fac18a2eeee2f0c50584ac
GreenBay777 <pronano12@gmail.com>
2024-05-08 11:35:31 -0400

    make tagName the parameter for tags endpoint instead of tag_id

--- backend/routes/tags.js
+++ backend/routes/tags.js
@@ -10,28 +10,50 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local');
 const {checkAuthenticated} = require('../middleware.js');
 
-// Follow tagId
-router.post('/follow/:tagId', checkAuthenticated, function (req, res) {
+
+// Follow a tag from the name
+router.post('/follow/:tagName', checkAuthenticated, function (req, res) {
   const userId = req.user.user_id;
-  const tagId = req.params.tagId;
+  const tagName = req.params.tagName;
 
   pool.query(
-    `INSERT INTO UserTag (user_id, tag_id)
-    VALUES (?, ?)
-    ON DUPLICATE KEY UPDATE tag_id = VALUES(tag_id);`,
-    [userId, tagId],
+    `SELECT tag_id FROM Tags WHERE tag_name = ?`,
+    [tagName],
     (error, results) => {
       if (error) {
-        console.error(`Error when user ${userId} is trying to follow tag ${tagId}`, error);
-        res.status(500).send('Error when trying to follow tag');
+        console.error(`Error finding tag_id for tagName ${tagName}`, error);
+        res.status(500).send('Error finding tag');
         return;
       }
-      res.status(201).send({
-        message: 'User is now following the tag',
-      });
-    },
+
+      if (results.length === 0) {
+        console.error(`Tag ${tagName} not found`);
+        res.status(404).send('Tag not found');
+        return;
+      }
+
+      const tagId = results[0].tag_id;
+
+      pool.query(
+        `INSERT INTO UserTag (user_id, tag_id)
+        VALUES (?, ?)
+        ON DUPLICATE KEY UPDATE tag_id = VALUES(tag_id);`,
+        [userId, tagId],
+        (error, results) => {
+          if (error) {
+            console.error(`Error when user ${userId} is trying to follow tag ${tagName}`, error);
+            res.status(500).send('Error when trying to follow tag');
+            return;
+          }
+          res.status(201).send({
+            message: 'User is now following the tag',
+          });
+        },
+      );
+    }
   );
 });
+
 
 
 // GET all tags a user is following
@@ -62,28 +84,48 @@ router.get('/', checkAuthenticated, function (req, res) {
 });
 
 // Unfollow tagId
-router.delete('/unfollow/:tagId', checkAuthenticated, function (req, res) {
+router.delete('/unfollow/:tagName', checkAuthenticated, function (req, res) {
   const userId = req.user.user_id;
-  const tagId = req.params.tagId;
+  const tagName = req.params.tagName;
 
   pool.query(
-    `DELETE FROM UserTag
-    WHERE user_id = ? AND tag_id = ?;`,
-    [userId, tagId],
+    `SELECT tag_id FROM Tags WHERE tag_name = ?`,
+    [tagName],
     (error, results) => {
       if (error) {
-        console.error(`Error when user ${userId} is trying to unfollow tag ${tagId}`, error);
-        res.status(500).send('Error when trying to unfollow tag');
+        console.error(`Error finding tag_id for tagName ${tagName}`, error);
+        res.status(500).send('Error finding tag');
         return;
       }
-      if (results.affectedRows === 0) {
-        res.status(404).send('User is not following tag');
+
+      if (results.length === 0) {
+        console.error(`Tag ${tagName} not found`);
+        res.status(404).send('Tag not found');
         return;
       }
-      res.status(201).send({
-        message: 'User is now unfollowing the tag',
-      });
-    },
+
+      const tagId = results[0].tag_id;
+
+      pool.query(
+        `DELETE FROM UserTag
+        WHERE user_id = ? AND tag_id = ?;`,
+        [userId, tagId],
+        (error, results) => {
+          if (error) {
+            console.error(`Error when user ${userId} is trying to unfollow tag ${tagName}`, error);
+            res.status(500).send('Error when trying to unfollow tag');
+            return;
+          }
+          if (results.affectedRows === 0) {
+            res.status(404).send('User is not following tag');
+            return;
+          }
+          res.status(200).send({
+            message: 'User is now unfollowing the tag',
+          });
+        },
+      );
+    }
   );
 });
 
